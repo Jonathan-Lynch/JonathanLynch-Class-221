25efa4c7f2c05fa41c8b159d08e5f51f3f16bccf
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-04-03 08:42:42 -0500

    My first push

---
+++ LCA-2020/LCA-2020/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManyMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string firstName = "Jonathan";
+            string middleName = ".C.";
+            string lastName = "Lynch";
+
+            string name = firstName + " " + middleName + lastName;
+
+            int size = name.Length;
+
+            Console.WriteLine(name);
+            Console.WriteLine("length is" + size);
+        }
+    }
+}
