4e7d8b98cca30fde3fa0ca1a9e327e8bc8e46858
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-04-14 13:26:09 -0500

    test

---
+++ LCA-2020-Class-221/Collections/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic; //important
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			stackPractice();
+
+		}
+
+		static void stackPractice()
+		{
+			Stack<string> colors = new Stack<string>();
+			colors.Push("Red");
+			colors.Push("Blue");
+			colors.Push("Yellow");
+
+			foreach(String color in colors)
+			{
+				Console.WriteLine(color);
+				//Console.ReadLine();
+			}
+			Console.ReadLine();
+
+			String Remove = colors.Pop();
+			Console.WriteLine("Just Removed {0}", Remove);
+			Console.WriteLine("My stack now has {0} elements", colors.Count());
+			Console.ReadLine();
+			// Peek
+		}
+
+	}
+}
