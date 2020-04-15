6ba0b2927dcdedd017b434a3ab68696b2f23015c
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-04-14 19:53:54 -0500

    oops

--- LCA-2020-Class-221/Presentation/Program.cs
+++ LCA-2020-Class-221/Presentation/Program.cs
@@ -9,13 +9,23 @@ namespace Presentation
 	class Program
 	{
 		/// <summary>
-		/// Git add (Specific Line)
+		/// Git add (Specific Line) not .
 		/// Git Log
 		/// Git revert
 		/// Git Reset
 		/// </summary>
 		static void Main(string[] args)
 		{
+			Console.WriteLine("dog");
+			Console.ReadLine();
+			Console.WriteLine("Cat");
+			Console.ReadLine();
+			string
+				str
+				rfjeorf
+				foirjofi
+				eiqofj[qo
+					qeroifj]
 		}
 	}
 }
