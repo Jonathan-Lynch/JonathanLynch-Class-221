934ae075a31d5f48b88f46ef1e664eb4d07ef501
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-04-14 19:57:27 -0500

    Revert "oops"
    This reverts commit 6ba0b2927dcdedd017b434a3ab68696b2f23015c.

--- LCA-2020-Class-221/Presentation/Program.cs
+++ LCA-2020-Class-221/Presentation/Program.cs
@@ -9,23 +9,13 @@ namespace Presentation
 	class Program
 	{
 		/// <summary>
-		/// Git add (Specific Line) not .
+		/// Git add (Specific Line)
 		/// Git Log
 		/// Git revert
 		/// Git Reset
 		/// </summary>
 		static void Main(string[] args)
 		{
-			Console.WriteLine("dog");
-			Console.ReadLine();
-			Console.WriteLine("Cat");
-			Console.ReadLine();
-			string
-				str
-				rfjeorf
-				foirjofi
-				eiqofj[qo
-					qeroifj]
 		}
 	}
 }
