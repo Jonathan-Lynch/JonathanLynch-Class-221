e4731c99998a874bca907ab95937d2d5f183fac0
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-04-27 13:57:58 -0500

    TOH

--- LCA-2020-Class-221/Tower_Of_Hanoi/Program.cs
+++ LCA-2020-Class-221/Tower_Of_Hanoi/Program.cs
@@ -8,56 +8,106 @@ namespace Tower_Of_Hanoi
 {
 	class Program
 	{
+		const string winning_combo = "1234";
 		static void Main(string[] args)
 		{
-			while (true)
+
+			//while (true)
+			//{
+
+			Dictionary<string, Stack<int>> towers = new Dictionary<string, Stack<int>>();
+			Stack<int> Disks = new Stack<int>();
+
+
+			Disks.Push(4);
+			Disks.Push(3);
+			Disks.Push(2);
+			Disks.Push(1);
+
+			towers.Add("A", Disks);
+			Disks = new Stack<int>();
+			towers.Add("B", new Stack<int>());
+			towers.Add("C", new Stack<int>());
+
+			do
 			{
 
-				Dictionary<string, Stack<int>> towers = new Dictionary<string, Stack<int>>();
-				Stack<int> Disks = new Stack<int>();
+				var firstTower = towers["A"];
+				var secondTower = towers["B"];
+				var thirdTower = towers["C"];
 
+				//int poppedItem = firstTower.Pop();
+				//secondTower.Push(poppedItem);
 
-				Disks.Push(1);
-				Disks.Push(2);
-				Disks.Push(3);
-				Disks.Push(4);
-
-				towers.Add("A:", Disks);
-				Disks = new Stack<int>();
-				towers.Add("B:", Disks);
-				towers.Add("C:", Disks);
-
-
+				Console.Clear();
 				foreach (var tow in towers)
 				{
-
 					var item_key = tow.Key;
 					string print_tower = item_key;
+					string print_number = "";
 
 					foreach (var stack_item in tow.Value)
 					{
-
-						print_tower += " " + stack_item.ToString();
-
+						print_number += " " + stack_item.ToString();
 					}
+					print_number = Reverse(print_number);
 
-					Console.WriteLine(print_tower);
-
+					Console.WriteLine(print_tower + ":" + print_number);
 				}
 
-				var moving = Disks;
+				bool invalid = true;
 
-				//Disks.Push(7);
-				var valueInStack = Disks.Pop();
-				Console.WriteLine(valueInStack.ToString());
+				do
+				{
 
-				Disks.Pop();
-				Console.ReadLine();
+					Console.WriteLine("Type in A, B, or C to target and move a disc");
+					var input = Console.ReadLine().ToUpper().ToUpper();
+					
 
-			}
+					Console.WriteLine("Type in where you want it to go");
+					var inputTo = Console.ReadLine().ToUpper();
+
+					int topToValue = 10;
+					int topFromValue = 10;
+
+					if (towers[inputTo].Count > 0)
+					{
+						topToValue = towers[inputTo].Pop();
+						towers[inputTo].Push(topToValue);
+					}
+
+					if (towers[input].Count > 0)
+					{
+						topFromValue = towers[input].Pop();
+						towers[input].Push(topFromValue);
+					}
+					Console.WriteLine("TOP TO: " + topToValue.ToString());
+					Console.WriteLine("TOP FROM: " + topFromValue.ToString());
+
+					if (topFromValue < topToValue)
+						towers[inputTo].Push(towers[input].Pop());
+					else
+						Console.WriteLine("Can't move there.");
+
+					Console.ReadLine();
+
+
+				} while (!invalid);
+
+
+			} while (true);
+
+
+			//Disks.Push(7);
+			//var valueInStack = Disks.Pop();
+			//Console.WriteLine(valueInStack.ToString());
+			//Disks.Pop();
+			//}
+
+
 			/*
 			Console.WriteLine("Type in A, B, or C to target and move a disc");
-			string input = Console.ReadLine().ToUpper();
+			string input = .ReadLine().ToUpper();
 
 			if ((input == "A") && (input == "B"))
 			{
@@ -66,5 +116,28 @@ namespace Tower_Of_Hanoi
 
 			Console.ReadLine(); */
 		}
+
+		public bool CheckWon(Stack<int> stackInt)
+		{
+			string StackValue = "";
+
+			foreach(var num in stackInt)
+			{
+				StackValue += num.ToString();
+			}
+
+			return (StackValue == winning_combo); 
+		}
+
+		public static string Reverse(string s)
+		{
+			char[] charArray = s.ToCharArray();
+			Array.Reverse(charArray);
+			return new string(charArray);
+		}
+
 	}
+
+
 }
+
