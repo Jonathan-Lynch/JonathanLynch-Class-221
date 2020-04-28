5c38ba4fb16c38b514526a2630cd68f503f8c63e
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-04-27 20:23:31 -0500

    ReadMe

---
+++ LCA-2020-Class-221/Tower_Of_Hanoi/ReadMe.cs
@@ -0,0 +1,19 @@
+﻿/*
+ 
+I Need 3 pillars A, B, and C.
+
+    A:
+    B:
+    C:
+ 
+Pillar A needs 1, 2, 3, and 4 disks reverse.
+and the rest need to be blank
+
+    A: 4 3 2 1
+    B:
+    C:
+
+Larger numbers cannot be stacked on top of smaller numbers 
+ If Pillar B or C have the Disks lined up '4 3 2 1' then declare winner
+
+ */
