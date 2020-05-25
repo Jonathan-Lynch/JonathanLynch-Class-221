d22f9c9c006090b9383c84fc7f42bea4eef572cd
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-05-24 21:52:36 -0500

    Worked on ToDoApp

---
+++ LCA-2020-Class-221/ToDoListApp/ItemContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+namespace ToDoApp
+{
+
+
+    class ItemContext : DbContext
+    {
+
+
+        // This property corresponds to the table in our database
+        public DbSet<ToDoItem> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // get the directory the code is being executed from
+            DirectoryInfo ExecutionDirectory = new DirectoryInfo(AppContext.BaseDirectory);
+
+            // get the base directory for the project
+            DirectoryInfo ProjectBase = ExecutionDirectory.Parent.Parent.Parent;
+
+            // add 'students.db' to the project directory
+            String DatabaseFile = Path.Combine(ProjectBase.FullName, "Items.db");
+
+            // to check what the path of the file is, uncomment the file below
+            //Console.WriteLine("using database file :"+DatabaseFile);
+
+            optionsBuilder.UseSqlite("Data Source=" + DatabaseFile);
+        }
+    }
+}
---
+++ LCA-2020-Class-221/ToDoListApp/ItemRepo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ToDoApp
+{
+    class ItemRepository
+    {
+        private ItemContext db;
+        public ItemRepository()
+        {
+            db = new ItemContext();
+            db.Database.EnsureCreated();
+
+        }
+        public void Create(ToDoItem item)
+        {
+            db.Items.Add(item);
+            db.SaveChanges();
+
+        }
+        public List<ToDoItem> GetAll()
+        {
+            return db.Items.ToList();
+        }
+        public List<ToDoItem> GetDoneItems()
+        {
+            return db.Items.Where(Item => Item.IsDone).ToList();
+        }
+        public List<ToDoItem> GetPendingItems()
+        {
+            return db.Items.Where(Item => Item.IsPending).ToList();
+        }
+        public void Delete(int Id)
+        {
+            ToDoItem item = db.Items.Find(Id);
+            db.Items.Remove(item);
+            db.SaveChanges();
+
+        }
+        public void MarkAsDone(int Item)
+        {
+            ToDoItem Existing = db.Items.Find(Item);
+            Existing.IsDone = true;
+            db.Update(Existing);
+            db.SaveChanges();
+        }
+    }
+}
