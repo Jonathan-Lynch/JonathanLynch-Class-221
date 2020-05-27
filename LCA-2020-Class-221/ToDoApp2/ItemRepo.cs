61561314c61b0f80d0dafcb791867281d444b0cb
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-05-27 17:31:38 -0500

    update

---
+++ LCA-2020-Class-221/ToDoApp2/ItemRepo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp2
+{
+	class ItemRepo
+	{
+		private ItemContext db;
+
+		public ItemRepo()
+		{
+			db = new ItemContext();
+			db.Database.EnsuredCreated();
+		}
+
+		public void Created(ToDoItem )
+		{
+
+		}
+
+	}
+}
